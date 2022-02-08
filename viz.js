66108834c17992b9eaa7f090adf611eab61c4176
LZachmann <LZachmann@gmail.com>
2022-02-08 22:10:16 +0000

    Updated viz.js (added 30 lines, deleted 2 lines).

--- viz.js
+++ viz.js
@@ -3,6 +3,9 @@
 
 var viz = {};
 viz.params = {}; // visualization parameter
+viz.chart = {};
+
+// ==== parameters ====
 
 viz.params.L8 = {
   bands: ["SR_B4", "SR_B3", "SR_B2"],
@@ -10,8 +13,33 @@ viz.params.L8 = {
   min: 1000,
   max: 65455,
   opacity: 1,
-}; // Define L8 visualization parameters
+};
 
 viz.params.sma = { bands: "water", gamma: 1, min: 0, max: 900, opacity: 1 };
+
+
+// ==== plotting ====
+
+viz.chart.vi = function(imageCollection, regions, band) {
+  return (
+    ui.Chart.image.seriesByRegion(	
+    imageCollection,	
+    regions,	
+    ee.Reducer.mean(),	
+    band,	
+    30,	
+    "system:time_start",	
+    "system:index"	
+  )	
+  .setChartType("ScatterChart")	
+  .setOptions({	
+    title: band,	
+    vAxis: { title: band },	
+    lineWidth: 1,	
+    pointSize: 4,	
+    colors: [color],	
+  })
+    );
+};
 
 exports = viz;
