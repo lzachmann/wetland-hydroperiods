003e2fba351259b6190e57c7f1d043e945f07c4e
LZachmann <LZachmann@gmail.com>
2022-02-08 22:57:18 +0000

    Updated src/viz.js (added 1 line, deleted 1 line).

--- src/viz.js
+++ src/viz.js
@@ -1,5 +1,5 @@
 // Bring this file into your scripts with:
-// var viz = require('users/laura_csp/wetland_hydroperiods:viz.js');
+// var viz = require('users/laura_csp/wetland_hydroperiods:src/viz.js');
 
 var viz = {};
 viz.params = {}; // visualization parameter
