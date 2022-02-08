df89257bf3d0e542210b886712671646c2973139
LZachmann <LZachmann@gmail.com>
2022-02-08 22:56:46 +0000

    Updated src/utils.js (added 1 line, deleted 1 line).

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,5 @@
 // Bring this file into your scripts with:
-// var utils = require('users/laura_csp/wetland_hydroperiods:utils.js');
+// var utils = require('users/laura_csp/wetland_hydroperiods:src/utils.js');
 
 var utils = {};
 // utils.dir = 'TBD'; // base path for all assets
