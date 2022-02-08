30ccfbe012a56a0b0bdf9f2de1af8eb08cbc4d80
LZachmann <LZachmann@gmail.com>
2022-02-08 21:51:14 +0000

    Updated viz.js (added 1 line, deleted 1 line).

--- viz.js
+++ viz.js
@@ -1,7 +1,7 @@
 // Bring this file into your scripts with:
 // var viz = require('users/laura_csp/wetland_hydroperiods:viz.js');
 
-viz = {};
+var viz = {};
 
 viz.L8_vizParams = {
   bands: ["SR_B4", "SR_B3", "SR_B2"],
