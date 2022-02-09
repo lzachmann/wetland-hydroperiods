32a236d9894731e0098ed8353041d74479d2127c
LZachmann <LZachmann@gmail.com>
2022-02-09 05:56:58 +0000

    Updated main.js (added 195 lines, deleted 196 lines).

--- main.js
+++ main.js
@@ -17,8 +17,8 @@ var focalArea2 = /* color: #98ff00 */ee.Geometry.Polygon(
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
 // Imports
 var utils = require("users/laura_csp/wetland_hydroperiods:src/utils.js");
-// print(utils.endmembers.get('dflt')) //getEndmembers('L5', useCustomEndMembers)
-print(utils.getEndmembers("L5", true))
+// print(utils.getEndmembers("L5", true))
+
 // /*
 // Author(s): Meghan Halabisky (mhalabisky@gmail.com)
 // Countributor(s): Laura Farwell (laura@csp-inc.org) and Luke Zachmann (luke@csp-inc.org)
@@ -99,228 +99,227 @@ Map.addLayer(L8_scene, utils.viz.params.L8, "Selected Landsat 8 scene");
 // ----------------------------------------------
 
 var smaAllL5 = imageL5.map(utils.smaUnmixFun("L5", useCustomEndMembers)).map(utils.cloudMask);
-print(smaAllL5)
-// // .map(utils.cloudUnmask) // should unmask NA values & replace w -9999 (for export)
-// // Run SMA function on L8 stack
-// var smaAllL8 = imageL8.map(utils.smaUnmixL8).map(utils.cloudMask);
-// // .map(utils.cloudUnmask) // should unmask NA values & replace w -9999 (for export)
-// // Merge SMA datasets.
-// var smaAll = ee.ImageCollection(smaAllL5.merge(smaAllL8));
-// var smaAll = smaAll.sort("date");
+// .map(utils.cloudUnmask) // should unmask NA values & replace w -9999 (for export)
+// Run SMA function on L8 stack
+var smaAllL8 = imageL8.map(utils.smaUnmixFun("L8", useCustomEndMembers)).map(utils.cloudMask);
+// .map(utils.cloudUnmask) // should unmask NA values & replace w -9999 (for export)
+// Merge SMA datasets.
+var smaAll = ee.ImageCollection(smaAllL5.merge(smaAllL8));
+var smaAll = smaAll.sort("date");
 
-// // Display SMA image - first date
-// print(smaAll, "sma all");
-// Map.addLayer(smaAll, utils.viz.params.sma, "water", false);
+// Display SMA image - first date
+print(smaAll, "sma all");
+Map.addLayer(smaAll, utils.viz.params.sma, "water", false);
 
-// // -------------------------------------------------------------------------
-// // Run Normalized Difference indices over image collection - time series
-// // -------------------------------------------------------------------------
+// -------------------------------------------------------------------------
+// Run Normalized Difference indices over image collection - time series
+// -------------------------------------------------------------------------
 
-// // Add indices (NDVI, NDMI, NDWI, MNDWI)
-// var L5index = imageL5
-//   .map(utils.cloudMask)
-//   .map(utils.addIndexL5)
-//   .map(utils.createTimeBand_indices);
+// Add indices (NDVI, NDMI, NDWI, MNDWI)
+var L5index = imageL5
+  .map(utils.cloudMask)
+  .map(utils.addIndexL5)
+  .map(utils.createTimeBand_indices);
 
-// var L8index = imageL8
-//   .map(utils.cloudMask)
-//   .map(utils.addIndexL8)
-//   .map(utils.createTimeBand_indices);
+var L8index = imageL8
+  .map(utils.cloudMask)
+  .map(utils.addIndexL8)
+  .map(utils.createTimeBand_indices);
 
-// var indicesAll = ee.ImageCollection(L5index.merge(L8index));
-// // print(indicesAll, 'all indices');
-// // var indicesAll = indicesAll.sort('CLOUD_COVER');
+var indicesAll = ee.ImageCollection(L5index.merge(L8index));
+// print(indicesAll, 'all indices');
+// var indicesAll = indicesAll.sort('CLOUD_COVER');
 
-// // -----------------------------------------------------------------
-// // Plotting for data exploration
-// // -----------------------------------------------------------------
+// -----------------------------------------------------------------
+// Plotting for data exploration
+// -----------------------------------------------------------------
 
-// // Define charts and print to Console.
+// Define charts and print to Console.
 
-// // Surface water area time series
-// var smaWater = ui.Chart.image
-//   .seriesByRegion(smaAll, AOI, ee.Reducer.sum(), waterSMA, 30, "date")
-//   //.setChartType('ScatterChart')
-//   .setOptions({
-//     title: "Hydrograph",
-//     vAxis: { title: "hydrograph" },
-//     hAxis: { title: "Date", format: "YYYY-MMM", gridlines: { count: 12 } },
-//     lineWidth: 1,
-//     pointSize: 4,
-//     colors: ["#0000FF"],
-//   });
-// print(smaWater, "Hydrograph");
+// Surface water area time series
+var smaWater = ui.Chart.image
+  .seriesByRegion(smaAll, AOI, ee.Reducer.sum(), waterSMA, 30, "date")
+  //.setChartType('ScatterChart')
+  .setOptions({
+    title: "Hydrograph",
+    vAxis: { title: "hydrograph" },
+    hAxis: { title: "Date", format: "YYYY-MMM", gridlines: { count: 12 } },
+    lineWidth: 1,
+    pointSize: 4,
+    colors: ["#0000FF"],
+  });
+print(smaWater, "Hydrograph");
 
-// // NDVI time series
-// var timeSeries_index1 = utils.viz.chart.nd(indicesAll, AOI, index1, "#0f8755");
-// print(timeSeries_index1, index1);
+// NDVI time series
+var timeSeries_index1 = utils.viz.chart.nd(indicesAll, AOI, index1, "#0f8755");
+print(timeSeries_index1, index1);
 
-// // NDWI time series
-// var timeSeries_index2 = utils.viz.chart.nd(indicesAll, AOI, index2, "#0000FF");
-// print(timeSeries_index2, index2);
+// NDWI time series
+var timeSeries_index2 = utils.viz.chart.nd(indicesAll, AOI, index2, "#0000FF");
+print(timeSeries_index2, index2);
 
-// // -----------------------------------------------------------------
-// // Gridmet & TerraClimate Data
-// // -----------------------------------------------------------------
+// -----------------------------------------------------------------
+// Gridmet & TerraClimate Data
+// -----------------------------------------------------------------
 
-// // Selected climate metrics user-defined at top of script
+// Selected climate metrics user-defined at top of script
 
-// // Load and filter Gridmet Drought data
-// var gridmet = ee
-//   .ImageCollection("GRIDMET/DROUGHT")
-//   .filterDate(startDate, endDate) // filter by date
-//   .filterBounds(AOI); // filter by polygon
+// Load and filter Gridmet Drought data
+var gridmet = ee
+  .ImageCollection("GRIDMET/DROUGHT")
+  .filterDate(startDate, endDate) // filter by date
+  .filterBounds(AOI); // filter by polygon
 
-// // PDSI time series
-// var pdsiSeries = utils.viz.chart.gmet(gridmet, geometry, climVar1); // TODO: why geometry and not AOI?
-// print(pdsiSeries, climVar1);
+// PDSI time series
+var pdsiSeries = utils.viz.chart.gmet(gridmet, geometry, climVar1); // TODO: why geometry and not AOI?
+print(pdsiSeries, climVar1);
 
-// // EDDI time series
-// var eddiSeries = utils.viz.chart.gmet(gridmet, AOI, climVar2);
-// print(eddiSeries, climVar2);
+// EDDI time series
+var eddiSeries = utils.viz.chart.gmet(gridmet, AOI, climVar2);
+print(eddiSeries, climVar2);
 
-// // Load and filter monthly TerraClimate data
-// var monthlyClim = ee.ImageCollection("IDAHO_EPSCOR/TERRACLIMATE")
-//   .filter(ee.Filter.date(startDate, endDate));
+// Load and filter monthly TerraClimate data
+var monthlyClim = ee.ImageCollection("IDAHO_EPSCOR/TERRACLIMATE")
+  .filter(ee.Filter.date(startDate, endDate));
   
-// var sweSeries = utils.viz.chart.tclim(monthlyClim, AOI, climVar3, startDate, endDate);
-// print(sweSeries, climVar3);
+var sweSeries = utils.viz.chart.tclim(monthlyClim, AOI, climVar3, startDate, endDate);
+print(sweSeries, climVar3);
 
-// var prSeries = utils.viz.chart.tclim(monthlyClim, AOI, climVar4, startDate, endDate);
-// print(prSeries, climVar4);
+var prSeries = utils.viz.chart.tclim(monthlyClim, AOI, climVar4, startDate, endDate);
+print(prSeries, climVar4);
 
-// // -----------------------------------------------------------------
-// // Export Data
-// // -----------------------------------------------------------------
+// -----------------------------------------------------------------
+// Export Data
+// -----------------------------------------------------------------
 
-// //  SMA
-// var smaSum = smaAll.map(function (i) {
-//   // Sum SMA proportions by wetland polygon
-//   return i.reduceRegions(AOI, ee.Reducer.sum());
-// });
-// // Flatten collection and remove geometry for export
-// var smaTimeSeries = smaSum.flatten().select([".*"], null, false);
-// // Export csv file of data summary to user Google Drive account
-// Export.table.toDrive(smaTimeSeries, "SMA_timeSeries");
+//  SMA
+var smaSum = smaAll.map(function (i) {
+  // Sum SMA proportions by wetland polygon
+  return i.reduceRegions(AOI, ee.Reducer.sum());
+});
+// Flatten collection and remove geometry for export
+var smaTimeSeries = smaSum.flatten().select([".*"], null, false);
+// Export csv file of data summary to user Google Drive account
+Export.table.toDrive(smaTimeSeries, "SMA_timeSeries");
 
-// // NDVI, NDWI
-// var indicesMean = indicesAll.map(function (i) {
-//   // Calculate mean Normalized index per wetland polygon
-//   return i.reduceRegions(geometry, ee.Reducer.mean());
-// });
-// // Flatten collection and remove geometry for export
-// var indicesTimeSeries = indicesMean.flatten().select([".*"], null, false);
-// // Export csv file
-// Export.table.toDrive(indicesTimeSeries, "Normalized_indices_timeSeries");
+// NDVI, NDWI
+var indicesMean = indicesAll.map(function (i) {
+  // Calculate mean Normalized index per wetland polygon
+  return i.reduceRegions(geometry, ee.Reducer.mean());
+});
+// Flatten collection and remove geometry for export
+var indicesTimeSeries = indicesMean.flatten().select([".*"], null, false);
+// Export csv file
+Export.table.toDrive(indicesTimeSeries, "Normalized_indices_timeSeries");
 
-// // Pixel count
-// var pixelQA = smaAll.select("QA_PIXEL");
-// // Count pixels per wetland polygon
-// var pixelCount = pixelQA.map(function (i) {
-//   return i.reduceRegions(geometry, ee.Reducer.count());
-// });
-// // Flatten collection and remove geometry for export
-// var pixelCountTimeSeries = pixelCount.flatten().select([".*"], null, false);
-// // Export csv file
-// Export.table.toDrive(pixelCountTimeSeries, "Pixel_count_timeSeries");
+// Pixel count
+var pixelQA = smaAll.select("QA_PIXEL");
+// Count pixels per wetland polygon
+var pixelCount = pixelQA.map(function (i) {
+  return i.reduceRegions(geometry, ee.Reducer.count());
+});
+// Flatten collection and remove geometry for export
+var pixelCountTimeSeries = pixelCount.flatten().select([".*"], null, false);
+// Export csv file
+Export.table.toDrive(pixelCountTimeSeries, "Pixel_count_timeSeries");
 
-// // --------------------------------------
-// // GRIDMET & TERRACLIMATE DATA EXPORTS
+// --------------------------------------
+// GRIDMET & TERRACLIMATE DATA EXPORTS
 
-// //  Extract and summarize GRIDMET drought metrics for export
-// var Gridmet = ee
-//   .ImageCollection("GRIDMET/DROUGHT")
-//   .filterDate(startDate, endDate) // filter by date
-//   .filterBounds(geometry); // filter by polygon
+//  Extract and summarize GRIDMET drought metrics for export
+var Gridmet = ee
+  .ImageCollection("GRIDMET/DROUGHT")
+  .filterDate(startDate, endDate) // filter by date
+  .filterBounds(geometry); // filter by polygon
 
-// // PDSI
-// var pdsiTimeSeries = Gridmet.map(function (image) {
-//   // Calculate mean PDSI per wetland polygon
-//   return image
-//     .select("pdsi")
-//     .reduceRegions({
-//       collection: geometry.select("pond_ID"),
-//       reducer: ee.Reducer.mean(),
-//       scale: 30,
-//     })
-//     .filter(ee.Filter.neq("mean", null))
-//     .map(function (f) {
-//       return f.set("date", image.id());
-//     });
-//   // Flatten collection and remove geometry for export
-// })
-//   .flatten()
-//   .select([".*"], null, false);
-// // Export csv file
-// Export.table.toDrive(pdsiTimeSeries, "PDSI_timeSeries");
+// PDSI
+var pdsiTimeSeries = Gridmet.map(function (image) {
+  // Calculate mean PDSI per wetland polygon
+  return image
+    .select("pdsi")
+    .reduceRegions({
+      collection: geometry.select("pond_ID"),
+      reducer: ee.Reducer.mean(),
+      scale: 30,
+    })
+    .filter(ee.Filter.neq("mean", null))
+    .map(function (f) {
+      return f.set("date", image.id());
+    });
+  // Flatten collection and remove geometry for export
+})
+  .flatten()
+  .select([".*"], null, false);
+// Export csv file
+Export.table.toDrive(pdsiTimeSeries, "PDSI_timeSeries");
 
-// // EDDI 1-yr
-// var eddiTimeSeries = Gridmet.map(function (image) {
-//   // Calculate mean EDDI per wetland polygon
-//   return image
-//     .select("eddi1y")
-//     .reduceRegions({
-//       collection: geometry.select("pond_ID"),
-//       reducer: ee.Reducer.mean(),
-//       scale: 30,
-//     })
-//     .filter(ee.Filter.neq("mean", null))
-//     .map(function (f) {
-//       return f.set("date", image.id());
-//     });
-//   // Flatten collection and remove geometry for export
-// })
-//   .flatten()
-//   .select([".*"], null, false);
-// // Export csv file
-// Export.table.toDrive(eddiTimeSeries, "EDDI-1yr_timeSeries");
+// EDDI 1-yr
+var eddiTimeSeries = Gridmet.map(function (image) {
+  // Calculate mean EDDI per wetland polygon
+  return image
+    .select("eddi1y")
+    .reduceRegions({
+      collection: geometry.select("pond_ID"),
+      reducer: ee.Reducer.mean(),
+      scale: 30,
+    })
+    .filter(ee.Filter.neq("mean", null))
+    .map(function (f) {
+      return f.set("date", image.id());
+    });
+  // Flatten collection and remove geometry for export
+})
+  .flatten()
+  .select([".*"], null, false);
+// Export csv file
+Export.table.toDrive(eddiTimeSeries, "EDDI-1yr_timeSeries");
 
-// //  Extract and summarize TERRACLIMATE metrics for export
-// var TerraClimate = ee
-//   .ImageCollection("IDAHO_EPSCOR/TERRACLIMATE")
-//   .filterDate(startDate, endDate) // filter by date
-//   .filterBounds(geometry); // filter by polygon
+//  Extract and summarize TERRACLIMATE metrics for export
+var TerraClimate = ee
+  .ImageCollection("IDAHO_EPSCOR/TERRACLIMATE")
+  .filterDate(startDate, endDate) // filter by date
+  .filterBounds(geometry); // filter by polygon
 
-// // SWE (snow water equiv)
-// var sweTimeSeries = TerraClimate.map(function (image) {
-//   // Calculate mean SWE per wetland polygon
-//   return image
-//     .select("swe")
-//     .reduceRegions({
-//       collection: geometry.select("pond_ID"),
-//       reducer: ee.Reducer.mean(),
-//       scale: 30,
-//     })
-//     .filter(ee.Filter.neq("mean", null))
-//     .map(function (f) {
-//       return f.set("date", image.id());
-//     });
-//   // Flatten collection and remove geometry for export
-// })
-//   .flatten()
-//   .select([".*"], null, false);
-// // Export csv file
-// Export.table.toDrive(sweTimeSeries, "SWE_timeSeries");
+// SWE (snow water equiv)
+var sweTimeSeries = TerraClimate.map(function (image) {
+  // Calculate mean SWE per wetland polygon
+  return image
+    .select("swe")
+    .reduceRegions({
+      collection: geometry.select("pond_ID"),
+      reducer: ee.Reducer.mean(),
+      scale: 30,
+    })
+    .filter(ee.Filter.neq("mean", null))
+    .map(function (f) {
+      return f.set("date", image.id());
+    });
+  // Flatten collection and remove geometry for export
+})
+  .flatten()
+  .select([".*"], null, false);
+// Export csv file
+Export.table.toDrive(sweTimeSeries, "SWE_timeSeries");
 
-// // PR (precip accum)
-// var prTimeSeries = TerraClimate.map(function (image) {
-//   // Calculate mean PR per wetland polygon
-//   return image
-//     .select("pr")
-//     .reduceRegions({
-//       collection: geometry.select("pond_ID"),
-//       reducer: ee.Reducer.mean(),
-//       scale: 30,
-//     })
-//     .filter(ee.Filter.neq("mean", null))
-//     .map(function (f) {
-//       return f.set("date", image.id());
-//     });
-//   // Flatten collection and remove geometry for export
-// })
-//   .flatten()
-//   .select([".*"], null, false);
-// // Export csv file
-// Export.table.toDrive(prTimeSeries, "PR_timeSeries");
+// PR (precip accum)
+var prTimeSeries = TerraClimate.map(function (image) {
+  // Calculate mean PR per wetland polygon
+  return image
+    .select("pr")
+    .reduceRegions({
+      collection: geometry.select("pond_ID"),
+      reducer: ee.Reducer.mean(),
+      scale: 30,
+    })
+    .filter(ee.Filter.neq("mean", null))
+    .map(function (f) {
+      return f.set("date", image.id());
+    });
+  // Flatten collection and remove geometry for export
+})
+  .flatten()
+  .select([".*"], null, false);
+// Export csv file
+Export.table.toDrive(prTimeSeries, "PR_timeSeries");
 
 //*/
